234a63f392b1127142bcbdecd7ba4e054ffc7cb4
Diego <diesalash@gmail.com>
2020-02-12 23:17:32 +0100

    Love is in the air

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,6 +69,9 @@ const IndexPage = () => (
     <div>
       This is a message from Perú: Buenos días a todos.
     </div>
+    <div>
+      Happy Valantine's day!
+    </div>
     <br />
     <UsersTable users={users} />
     <br />
