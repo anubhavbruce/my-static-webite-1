ea5e0577ab1f5078b85336ef840a31cf3d19dcd2
Emma Lundström <emma.lundstrom@unomaly.com>
2020-02-23 20:36:34 +0100

    follow the white rabbit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,77 +6,88 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import UsersTable from "../components/users-table"
 
-const users = [
-  {
-    fname: 'Marcel',
-    lname: 'Marcel',
-    nickname: '@mvuijk'
-  },
-  {
-    fname: 'Juan Carlos',
-    lname: 'Martinez',
-    nickname: '@jkmrto'
-  },
-  {
-    fname: 'Alejandro',
-    lname: 'Melo',
-    nickname: '@alxmlo'
-  },
-  {
-    fname: 'Pedro',
-    lname: 'Capdevila',
-    nickname: '@pcapdevila'
-  },
-  {
-    fname: 'Michel',
-    lname: 'Kreutner',
-    nickname: '@mik57'
-  },
-  {
-    fname: 'Artem',
-    lname: 'Holinka',
-    nickname: '@artem.holinka'
-  },
+const users = [{
+        fname: 'Marcel',
+        lname: 'Marcel',
+        nickname: '@mvuijk'
+    },
     {
-    fname: 'Aline',
-    lname: 'Aline',
-    nickname: '@aline.test'
-  }
+        fname: 'Juan Carlos',
+        lname: 'Martinez',
+        nickname: '@jkmrto'
+    },
+    {
+        fname: 'Alejandro',
+        lname: 'Melo',
+        nickname: '@alxmlo'
+    },
+    {
+        fname: 'Pedro',
+        lname: 'Capdevila',
+        nickname: '@pcapdevila'
+    },
+    {
+        fname: 'Michel',
+        lname: 'Kreutner',
+        nickname: '@mik57'
+    },
+    {
+        fname: 'Artem',
+        lname: 'Holinka',
+        nickname: '@artem.holinka'
+    },
+    {
+        fname: 'Aline',
+        lname: 'Aline',
+        nickname: '@aline.test'
+    }
 ]
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <ul>
-      <li>My own merge request</li>
-      <li>This is a new title</li>
-      <li>New li added here</li>
-      <li>This is another new li :P</li>
-      <li>And another new li to test 2</li>
-      <li>Wake up Neo ! ... The Matrix has you.</li>
-    </ul>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>this is the paragraph added by a participant</p>
-    <p>Now go build something great.</p>
-    <h4>Who has changed this page:</h4>
-    <div>
-        Adding simple paragraph before the table
-    </div>
-    <div>
-      Little hello from France !... Have a nice day ...
-    </div>
-    <div>
-      This is a message from Perú: Buenos días a todos.
-    </div>
-    <br />
-    <UsersTable users={users} />
-    <br />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+const IndexPage = () => ( <
+    Layout >
+    <
+    SEO title = "Home"
+    keywords = {
+        [`gatsby`, `application`, `react`] }
+    /> <
+    h1 > Hi people < /h1> <
+    ul >
+    <
+    li > My own merge request < /li> <
+    li > This is a new title < /li> <
+    li > New li added here < /li> <
+    li > This is another new li: P < /li> <
+    li > And another new li to test 2 < /li> <
+    li > Wake up Neo!...The Matrix has you. < /li> <
+    li > Follow the white rabbit. < /li> <
+    /ul> <
+    p > Welcome to your new Gatsby site. < /p> <
+    p > this is the paragraph added by a participant < /p> <
+    p > Now go build something great. < /p> <
+    h4 > Who has changed this page: < /h4> <
+    div >
+    Adding simple paragraph before the table <
+    /div> <
+    div >
+    Little hello from France!...Have a nice day... <
+    /div> <
+    div >
+    This is a message from Perú: Buenos días a todos. <
+    /div> <
+    br / >
+    <
+    UsersTable users = { users }
+    /> <
+    br / >
+    <
+    div style = {
+        { maxWidth: `300px`, marginBottom: `1.45rem` } } >
+    <
+    Image / >
+    <
+    /div> <
+    Link to = "/page-2/" > Go to page 2 < /Link> <
+    /Layout>
 )
 
 export default IndexPage
