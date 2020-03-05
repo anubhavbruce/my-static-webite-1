982a24e146c3b2a99ce9f728449d17a8445dce41
shainu begum <shainubegum786@gmail.com>
2020-03-05 02:36:50 +0000

    added paragraph in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,6 +65,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>this is the paragraph added by a participant</p>
     <p>Now go build something great.</p>
+    <p>This is a paragraph added as part of assignment </p>
     <h4>Who has changed this page:</h4>
     <div>
         Adding simple paragraph before the table
