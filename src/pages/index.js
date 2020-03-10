6b6682c2a583fd98333c7b828ee13a1735ed51ad
Kirilov <setdream@wisebits.com>
2020-03-10 16:56:30 +0200

    Add new paragraph on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,6 +65,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>this is the paragraph added by a participant</p>
     <p>Now go build something great.</p>
+    <p>This is my new paragraph.</p>
     <h4>Who has changed this page:</h4>
     <div>
         Adding simple paragraph before the table
