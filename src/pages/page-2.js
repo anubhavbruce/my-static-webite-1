9837e67e4b7c644365f880640b83f176684987e7
Dmitry Kostjuchenko <kdp.029029@gmail.com>
2020-03-26 09:46:59 +0000

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,7 +9,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <p>Don't forget to your hands with soap.</p>
+    <p>Don't worry, be happy.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
