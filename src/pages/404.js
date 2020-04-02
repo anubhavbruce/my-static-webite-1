b0a1f17e81f66ba4449681da2a9f52a2c39b653f
Magnus Gedda <magnus.gedda@vironova.com>
2020-04-02 15:27:19 +0200

    Add paragraph to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,6 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
+    <p>Well this was embarrasing for us.</p>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     <div>Try another <a href="https://www.gitlab.com">link</a></div>
   </Layout>
