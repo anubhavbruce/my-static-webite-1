3d8ada2fbc2aac05c8cdb8bb244c5aa7c8fd54d9
Nguyen Trong Kha <nguyentrongkha@gmail.com>
2020-05-20 00:30:19 +0000

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children }) => (
             <div>Version: %%VERSION%%</div>
             © {new Date().getFullYear()}, Built with
             {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <a href="https://www.gatsbyjs.org">Gatsby @ React Framework</a>
           </footer>
         </div>
       </>
