aa0db1e6cb7b13d559c395047fbea0fa465d362d
Shigeya Tanabe <st_gitlab@namio.jp>
2020-08-14 04:08:02 +0000

    Update index.js, Added another new ST title.

--- src/pages/index.js
+++ src/pages/index.js
@@ -176,6 +176,7 @@ const IndexPage = () => (
     <h2>Nice today!</h2>
     <h3>This is a other NEW TITLE</h3>
     <h3>This is a new Pavo title</h3>
+    <h3>This is another new ST title</h3>
     <h4>Who has changed this page:</h4>
     <p>Someone changed the title!</p>
     <p>It was not me :) </p>
