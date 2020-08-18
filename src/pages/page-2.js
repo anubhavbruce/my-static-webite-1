64999bc90234eea7af1b3d30c7c9f29ff2ed12a0
Juan Ismael Vasquez <ismael.juan@gmail.com>
2020-08-18 12:07:26 -0300

    Updating the page 2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,7 +8,8 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h2>Welcome to page 2</h2>
+    <p>Complete the page body here</p>	
     <p>Don&apos;t forget to wash your hands with soap.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
