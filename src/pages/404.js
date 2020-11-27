57b43da359c0513887433c6ccfc36af172eca1e8
Anna Perez <annavperez@gmail.com>
2020-11-27 15:55:36 -0600

    Add small change

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Page Not found!" />
     <h1>NOT FOUND</h1>
-    <p>Well this was embarrasing for us.</p>
+    <p>Well this was embarrasingly small error for us.</p>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     <p>Please note that this webpage is under construction so no suprise it does not work well</p>
     <div>Try another <a href="https://www.gitlab.com">link</a></div>
