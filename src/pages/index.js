923ce3e6cbbbe6aab41d6be73707ed63b9b94896
orhankalayci-devopscu <orhan@kalayci.com>
2020-12-14 14:55:09 +0000

    Update index.js Added my name to the list of users

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,11 @@ import UsersTable from "../components/users-table"
 
 const users = [
   {
+    fname: 'Orhan',
+    lname: 'KALAYCI',
+    nickname: '@orhankalayci-devopscu'
+  },
+  {
     fname: 'Karel',
     lname: 'Lujan',
     nickname: '@kalujan'
