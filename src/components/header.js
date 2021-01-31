041c6c9509b3eeeb21f0e3a71501f2387f8e50cd
Serafin Bäbler <serafin.baebler@gmail.com>
2021-01-31 13:28:45 +0000

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `burlywood`,
       marginBottom: `1.45rem`,
     }}
   >
