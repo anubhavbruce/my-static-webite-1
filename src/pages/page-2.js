3ad6d4fc2cde1d30e49a38508b608e2977b283eb
NickMaynard <nick.maynard@cityscience.com>
2021-05-06 19:58:26 +0100

    -Add tag to page 2 ;)

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,6 +9,7 @@ const SecondPage = () => (
     <SEO title="Useful life hacks" />
     <h1>Hi from the second page</h1>
     <h2>Welcome to page 2</h2>
+    <h3>mayberts was here</h3>
     <p>Complete the page body here</p>	
     <p>Don&apos;t forget to wash your hands with soap.</p>
     <Link to="/">Go back to the homepage</Link>
