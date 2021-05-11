3ec21b91b89f3f69908dd6079fef3f2c4e8ee3bc
xing liu <xing.liu@gmx.de>
2021-05-11 16:21:02 +0200

    add birdie's greeting

--- src/pages/index.js
+++ src/pages/index.js
@@ -279,6 +279,7 @@ const IndexPage = () => (
     <p>Greetings from North America</p>
     <p>Greetings from sunny Manchester, UK</p>
     <p>Hello my friend, stay awhile and listen.</p>
+    <p>it is me, ur birdie</p>
     <section><p>Here is a new information to be learn.</p></section>
     <UsersTable users={users} />
   </Layout>
