263aae9fd9546d1e204dc6e54c084a511ee722ce
Ratakondala Arun <4882842-RatakondalaArun@users.noreply.gitlab.com>
2021-05-21 07:45:23 +0000

    Added emojis

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,11 +6,11 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Page Not found!" />
-    <h1>NOT FOUND</h1>
+    <h1>NOT FOUND🙈</h1>
     <p>Well this was embarrasingly small error for us.</p>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <p>Please note that this webpage is under construction so no suprise it does not work well</p>
-    <div>Try another <a href="https://www.gitlab.com">link</a></div>
+    <p>You just hit a route🛣 that doesn&#39;t exist... the sadness.☹</p>
+    <p>Please note that this webpage is under construction🚧 so no suprise it does not work well</p>
+    <div>Try another <a href="https://www.gitlab.com">🔗link</a></div>
     <p>And never lose hope</p>
   </Layout>
 )
