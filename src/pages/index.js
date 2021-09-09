9daafc07f9f6516332bf6e7767714913fb38f73e
Aleksey Balandin <murz29@gmail.com>
2021-09-09 08:05:11 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -336,6 +336,7 @@ const IndexPage = () => (
     <UsersTable users={users} />
     <p>Greetings from Montreal!</p>
     <p>Greetings from Moscow, Russia!</p>
+    <p>Greetings from Saint-Petersburg, Russia!</p>
     <p>Greetings from Moscow, I wish you all happiness, peace and health!!</p>
   </Layout>
 )
