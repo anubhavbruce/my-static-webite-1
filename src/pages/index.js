944f76173f538c9a4e36c42d797ecfaae0cbd2ae
rajarz <rajaram.rahul91@gmail.com>
2021-10-10 16:35:01 +1100

    made minor change within branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -348,6 +348,7 @@ const IndexPage = () => (
     <p>Greetings from Montreal!</p>
     <p>Greetings from Moscow, Russia!</p>
     <p>Greetings from Saint-Petersburg, Russia!</p>
+    <p>Greetings from Australia!</p>
     <p>Greetings from Moscow, I wish you all happiness, peace and health!!</p>
     <p>Greetings from India, I wish you all happiness, peace and health!!</p>
   </Layout>
