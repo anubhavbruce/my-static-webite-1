1c8f22aed2c257481b2e4699c7baf602f4135edc
Roman <rrrommmzzzesss@gmail.com>
2021-11-06 12:36:21 +0000

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          My little change
         </Link>
       </h1>
     </div>
