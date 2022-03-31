5d9b7114fe90fbe6c5b7eec186319322ced15555
Kimi Samuer <lucidog@protonmail.com>
2022-03-31 18:37:30 +0300

    New heading in 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,6 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Page Not found!" />
     <h1>NOT FOUND🙈</h1>
+    <h2>I hoped to find it, but I couldn't.</h2>
     <p>Well this was embarrasingly small error for us.</p>
     <p>You just hit a route🛣 that doesn&#39;t exist... the sadness.☹</p>
     <p>Please note that this webpage is under construction🚧 so no suprise it does not work well</p>
