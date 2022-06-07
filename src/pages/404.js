a4e524bf8a14846e77e49701b1610d08370a64ad
Karla Santos <7309960-kcstec7@users.noreply.gitlab.com>
2022-06-07 20:25:13 +0000

    Update src/pages/404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Page Not found!" />
     <h1>NOT FOUND🙈</h1>
-    <h2>I hoped to find it, but I couldn't.</h2>
+    <h2>Oh, no! I hoped to find it, but I couldn't.</h2>
     <p>Dalton</p>
     <p>Well this was embarrasingly small error for us.</p>
     <p>You just hit a route🛣 that doesn&#39;t exist... the sadness.☹</p>
