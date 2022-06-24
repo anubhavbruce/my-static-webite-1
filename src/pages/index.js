54a7f7263e5770f0e3f0dc20ebc16b1741876dd8
tomart1 <t_goebel@gmx.net>
2022-06-24 06:58:24 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -447,6 +447,7 @@ const IndexPage = () => (
   <p>This message will self destruct in 5 ... 4 ... 3 ...</p>
   <p>Greeting from A Coruña, Spain</p>
   <p><i>Quidquid latine dictum sit, altum videtur</i></p>
+  <p> Greetings from Bavaria </p>
   </Layout>
 )
 
