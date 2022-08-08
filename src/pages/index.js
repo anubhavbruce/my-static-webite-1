c76a3f23f2dab84d5fe61761f8a9971a7588ea22
Hector <hector.manrique@free-now.com>
2022-08-08 08:40:40 +0200

    Added Greetings from Spain to index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -463,6 +463,7 @@ const IndexPage = () => (
   <p><i>Quidquid latine dictum sit, altum videtur</i></p>
     <p>Greetings from Singapore and also meow </p>
   <p>Greetings from Italy</p>
+  <p>Hello from Spain!</p>
   </Layout>
 )
 
