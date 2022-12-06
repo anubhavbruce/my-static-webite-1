00e96f96a62d80d0491b2c23a27ee718048a8c93
Daniel Lenz <daniel.michael.lenz@gmail.com>
2022-12-06 19:10:52 +0000

    Update src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -343,6 +343,7 @@ const IndexPage = () => (
     <h1>Michael Jackson was here</h1>
     <h1>Checkout my Mixcloud "https://www.mixcloud.com/rui_duarte/"</h1>
     <h1>Hi people of the world from India!! :)</h1>
+    <h1>Who will win the World Cup?</h1>
     <h2>PloPlu</h2>
   <p>Greeting from India!!</p>
   <p>Merry Christmas 2021 :P</p>
