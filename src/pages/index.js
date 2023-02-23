774443fefcf5fed129be501976a3fe019a48f8e3
Kaoutar Eddriouich <kaoutar.eddriouich@gmail.com>
2023-02-23 19:50:58 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -403,7 +403,8 @@ const IndexPage = () => (
 
     <h1>Hi people</h1>
     <h1>I'm Ali from Turkey :)</h1>
-    <h3>guys, nice to meet you here!</h3>    
+    <h3>guys, nice to meet you here!</h3>   
+      <p>do you know that you are the most beautiful person in this world? </p> 
 
 
   <p>welcome to awesome gitlab tutorial</p>
