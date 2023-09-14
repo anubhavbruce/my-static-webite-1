1e55ac3a6bd5147f007858a813320e290c604ea5
Zaw Phyo <snippyjustro@gmail.com>
2023-09-14 07:31:44 +0630

    Update page name

--- src/pages/page-1.js
+++ src/pages/page-1.js
@@ -8,7 +8,7 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Useful life hacks" />
     <h1>Hi from the second page</h1>
-    <h2>Welcome to page 2</h2>
+    <h2>Welcome to page 1</h2>
     <h3>mayberts was here</h3>
     <h3>This is a H3 title</h3>
     <p>Complete the page body here</p>
